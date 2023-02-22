bdd5e2fa7fb2448133511829659c5aa05dff9afb
Kharlamov Victor <43378234+Xapulc@users.noreply.github.com>
2023-02-22 14:18:47 +0300

    Create solution.py

---
+++ solution.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+
+
+chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+
+def solution(x: np.array) -> float:
+    # Измените код этой функции
+    # Это будет вашим решением
+    # Не меняйте название функции и её аргументы
+    return x.mean() # Ваш ответ
