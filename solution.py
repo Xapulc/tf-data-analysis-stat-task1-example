6d499e7cf9fe9ca2fe77d6b09f6b0ee67e0d2ef1
Khachatur <khachkara@gmail.com>
2023-04-02 23:18:50 +0300

    solution test1
    Карапетян Хачатур

--- solution.py
+++ solution.py
@@ -1,11 +1,9 @@
 import pandas as pd
 import numpy as np
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 42791670 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    ln_checks = np.log(x)
+    alpha_hat = np.exp(np.mean(ln_checks))
+    return alpha_hat
