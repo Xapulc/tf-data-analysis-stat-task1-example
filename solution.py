0ccfe050c49c811ed14e7ec3c30682b2a18c5780
Jereinette <114301591+Jereinette@users.noreply.github.com>
2023-04-05 15:54:38 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,11 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 402739329 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    t = 4
+    return np.exp(- np.sqrt( x.mean() / t)) # Ваш ответ
